b7abc95dbcd8aa85d81eb56c315cac6e02f612a0
CMIW <pbl9815@gmail.com>
2020-12-27 21:26:30 -0600

    Cookie Session and sending userName with the socket

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+const session = require('cookie-session');
+const sessionMiddleware = session({secret:"ssshhhh", signed: true, resave: true, saveUninitialized: true});
+
 const http = require('http');
 const server = http.createServer(app);
 
@@ -11,15 +14,28 @@ const io = socketio(server, {cors:{origin:'*'}});
 // Port number
 const PORT = 3000 || process.env.PORT;
 
+// Socket Session manager
+io.use(function(socket, next){
+  sessionMiddleware(socket.request, socket.request.res || {}, next);
+});
+
+app.use(sessionMiddleware);
+
 // Run when client connects
 io.on('connection', function(socket) {
   console.log("New connection ...",socket.id);
+  //console.log(socket.request.session);
+
+  socket.on('userName', function (data) {
+      console.log(data);
+   });
 
   socket.on('disconnect', function () {
       console.log('A user disconnected');
    });
 });
 
+// Server listener
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- Frontend/src/app/layout/home/home.component.ts
+++ Frontend/src/app/layout/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SocketService } from '../../core/services/socket/socket.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,14 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public name: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private socketService: SocketService) { }
 
   ngOnInit(): void {
   }
 
   joinGroup(){
     this.router.navigate(['/join']);
+  }
+
+  createGroup(){
+    console.log(this.name);
+    this.socketService.emit("userName",this.name);
+    this.router.navigate(['/room']);
   }
 
 }
