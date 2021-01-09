4f09062f3426f0d379512bf078d3a5fcfbfa4369
CMIW <pbl9815@gmail.com>
2021-01-09 13:22:28 -0600

    Added Sessions Service class to manage user sessions

--- Backend/index.js
+++ Backend/index.js
@@ -8,32 +8,35 @@ const server = http.createServer(app);
 const socketio = require('socket.io');
 const io = socketio(server, {cors:{origin:'*'}});
 
+const SessionService = require('./services/session.service');
+const sessionService = new SessionService();
+
 // Port number
 const PORT = 3000 || process.env.PORT;
-
-var sessions = new Map();
 
 // Run when client connects
 io.on('connection', function(socket) {
   io.to(socket.id).emit('welcome', {});
 
-  // assigns the new socket id to the user token
-  socket.on('welcome', function (data) {
-    sessions[data.token] = socket.id;
+  // assigns token to new users or assing the new socket id for the  refreshed user token
+  socket.on('welcome', function (payload) {
+    if(payload.token == null){
+      sessionService.addNewSession({socketId:socket.id});
+      payload.token = socket.id;
+      io.to(socket.id).emit('handshake', payload);
+    }
+    else {
+      sessionService.updateSession(payload.token,socket.id);
+    }
   });
 
-  socket.on('handshake', function (data) {
-     data.token = socket.id;
-     sessions[socket.id] = socket.id;
-     io.to(socket.id).emit('handshake', data);
-  });
-
-  socket.on('userName', function (data) {
+  socket.on('userName', function (payload) {
+    sessionService.nameUserSession(payload.token, payload.data);
     //console.log(data);
   });
 
-  socket.on('test', function (data) {
-    console.log(data);
+  socket.on('test', function (payload) {
+    console.log(sessionService.getSession(payload.token));
   });
 
   socket.on('disconnect', function () {
